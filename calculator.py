2703c9fd43b96b09fe1902d58cb109c64c1681d5
Lucy Gavrich <annalgav@gmail.com>
2022-08-10 17:28:39 -0700

    create variables for num1, num2, create error messages for invalid inputs

--- calculator.py
+++ calculator.py
@@ -14,36 +14,62 @@ while True:
     # quit if first token is q
     if tokens[0] == "q":
         break
-    # if first token isn't q, check operator validity
-    elif tokens[0] in ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']:
-        # if operator is +, print sum of nums
-        if tokens[0] == "+":
-            print(add(float(tokens[1]), float(tokens[2])))
-        # if operator is -, print num1 - num2
-        elif tokens[0] == "-":
-            print(subtract(float(tokens[1]), float(tokens[2])))
-        # if operator is *, print num1 * num2
-        elif tokens[0] == "*":
-            print(multiply(float(tokens[1]), float(tokens[2])))
-        # if operator is /, print num1 / num2
-        elif tokens[0] == "/":
-            print(divide(float(tokens[1]), float(tokens[2])))
-        # if operator is square, print num1 ** 2
-        elif tokens[0] == "square":
-            print(square(float(tokens[1])))
-        # if operator is cube, print num1 ** 3
-        elif tokens[0] == "cube":
-            print(cube(float(tokens[1])))
-        # if operator is pow, print num1 ** num2
-        elif tokens[0] == "pow":
-            print(power(float(tokens[1]), float(tokens[2])))
-        # if operator is mod, print num1 % num2
-        elif tokens[0] == "mod":
-            print(mod(float(tokens[1]), float(tokens[2])))
+    # check for length of 2
+    if len(tokens) == 2:
+        # convert num1 to int
+        try:
+            float(tokens[1])
+        except:
+            print("ERROR: That's not a valid number")
+        else:
+            num1 = float(tokens[1])
+            # if operator is square, print num1 ** 2
+            if tokens[0] == "square":
+                print(square(num1))
+            # if operator is cube, print num1 ** 3
+            elif tokens[0] == "cube": 
+                print(cube(num1))
+            else:
+                print("ERROR: That's not a valid operator")
+    # check for length of 3
+    elif len(tokens) == 3:
+        # convert num1 to int
+        try:
+            float(tokens[1])
+        except:
+            print("ERROR: That's not a valid number")
+        else:
+            try:
+                float(tokens[2])
+            except:
+                print("ERROR: That's not a valid number")
+            else:
+                num1 = float(tokens[1])
+                num2 = float(tokens[2])
+                # if operator is +, print sum of nums
+                if tokens[0] == "+":
+                    print(add(num1, num2))
+                # if operator is -, print num1 - num2
+                elif tokens[0] == "-":
+                    print(subtract(num1, num2))
+                # if operator is *, print num1 * num2
+                elif tokens[0] == "*":
+                    print(multiply(num1, num2))
+                # if operator is /, print num1 / num2
+                elif tokens[0] == "/":
+                    print(divide(num1, num2))
+                # if operator is pow, print num1 ** num2
+                elif tokens[0] == "pow":
+                    print(power(num1, num2))
+                # if operator is mod, print num1 % num2
+                elif tokens[0] == "mod":
+                    print(mod(num1, num2))
+                # if operator not valid, print error
+                else:
+                    print("ERROR: That's not a valid operator")
 
-    # if operator not valid, print error
     else:
-        print("ERROR: That's not a valid operator")
-        
+        print("ERROR: That's an invalid input")
+            
             
     
